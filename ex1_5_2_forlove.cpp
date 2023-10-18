1c0afa792108e58187f54309b6f46b34dc683f0d
dikau <166174004@qq.com>
2023-10-18 20:54:35 +0800

    修改cpp

--- ex1_5_2_forlove.cpp
+++ ex1_5_2_forlove.cpp
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
 #include <iostream>
 
 int main()
 {
 	for (int i = 1; i <= 3000; ++i)
 	{
-		//I love you three thousand.
+		// I love you three thousand.
 		std::cout << "love you " << i << std::endl;
 	}
 	return 0;
-=======
-#include <iostream>
-
-int main()
-{
-	for (int i = 1; i <= 3000; ++i)
-	{
-		//I love you three thousand.
-		std::cout << "love you " << i << std::endl;
-	}
-	return 0;
->>>>>>> 44f35265ae4d55bf38be6fd8e076955e7bbec9fd
-}
